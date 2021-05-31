Transitioned to quaternions, fixed quantiles

main.py
@@ -1,11 +1,13 @@
 import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
+import pyquaternion as qt
 from tqdm import tqdm as tqdm
 from datetime import datetime
 from math import cos, sin, asin, acos, atan2, tan
 from math import radians as mrad
 from numba import njit, jit
+
 
 fileName = "Data_new_encoding.csv"
 
@@ -43,15 +45,30 @@ def anglesToVector(ra:np.ndarray, dec:np.ndarray) -> np.ndarray:
     """Convert angles to unit vectors
 
     Args:
-        ra (np.ndarray): ascenision
+        ra (np.ndarray): right ascension
         dec (np.ndarray): declination
 
     Returns:
         np.ndarray: vector
     """
+
+    unit_vector = np.array([1,0,0])
     ra = np.radians(ra)
     dec = np.radians(dec)
-    return np.array([np.cos(ra) * np.cos(dec), np.sin(ra), -np.cos(ra)*np.sin(dec)]).T
+
+    vectors = np.zeros([len(ra),3])
+
+    for i in range(len(ra)):
+
+        q1 = qt.Quaternion(axis=[0,0,1],angle = ra[i])
+        q2 = qt.Quaternion(axis=[0,1,0], angle = dec[i])
+        q3 = q1*q2
+        vector = q3.rotate(unit_vector)
+        vectors[i] = vector
+
+    return vectors
+
+    #return np.array([np.cos(ra) * np.cos(dec), np.sin(ra), -np.cos(ra)*np.sin(dec)]).T
 
 
 def getGamma(tel:np.ndarray, exos:np.ndarray) -> np.ndarray:
@@ -73,7 +90,7 @@ data = pd.read_csv(fileName, sep=',', skiprows=38)
 exo_data = data[['ra', 'dec', 'pl_name']]
 
 print(f"Total exoplanets = {len(exo_data)}")
-print(f"Total exoplanets below 0 dec = {len(exo_data[exo_data['dec'] < 3])}")
+print(f"Total exoplanets below 18 dec = {len(exo_data[exo_data['dec'] < 18])}")
 
 # exo_data = exo_data[exo_data.dec <= 3]
 exo_ra = exo_data.iloc[:, 0].to_numpy()
@@ -93,24 +110,39 @@ rTelescope = anglesToVector(raBasic, decMoon)
 rExos = anglesToVector(exo_ra, exo_dec).T
 
 
-rTelescope2 = rTelescope.T
+
+# 3D Projection stuff?
+'''rTelescope2 = rTelescope.T
 print(rTelescope2[0])
 origin = np.shape(rTelescope2)
+print('Origin:')
+print(origin)
 
 ax = plt.figure().add_subplot(projection='3d')
 ax.quiver(rTelescope2[0], rTelescope2[1], rTelescope2[2], origin[0], origin[1], origin[2])
 plt.show()
 # print(f"rTelescope = {rTelescope[4]}")
-# print(f"rExos = {rExos.T[1]}")
+# print(f"rExos = {rExos.T[1]}")'''
 
+
+#----------------Temporarily out of commission------------------
 gamma = getGamma(rTelescope, rExos)
 gammaMin = pd.DataFrame([np.min(gamma, axis=0)], columns=exo_names)
 gammaMax = pd.DataFrame([np.max(gamma, axis=0)], columns=exo_names)
 gammaDiff = pd.DataFrame([np.max(gamma, axis=0)- np.min(gamma, axis=0)], columns=exo_names)
+#gammeAverage = pd.DataFrame([np.])
 # print(f"gamma = {gamma[4, 1]}")
 
-
 gammaDF = pd.DataFrame(gamma, columns=exo_names)
+
+'''gamma = getGamma(rTelescope, rExos)
+gammaDF = pd.DataFrame(gamma, columns=exo_names)
+gammaMin = gammaDF.min(axis=0)
+gammaMax = gammaDF.max(axis=0)
+gammaDiff = gammaMax-gammaMin'''
+
+
+
 
 # print(gammaDF)
 
@@ -119,6 +151,7 @@ def thingy(qquantile, colorBar):
     exoQuantile = exoQuantile.append(gammaMin.iloc[0].rename("Min"), ignore_index=False)
     exoQuantile = exoQuantile.append(gammaMax.iloc[0].rename("Max"), ignore_index=False)
     exoQuantile = exoQuantile.append(gammaDiff.iloc[0].rename("Diff"), ignore_index=False)
+    #exoQuantile = exoQuantile.append()
 
     for angle in [x*barReso for x in range(0,(int(90/barReso) + 1))]:
         exoQuantile = exoQuantile.append(exoQuantile.loc["Quantile"].rename(f"{angle}") <= angle, ignore_index=False)